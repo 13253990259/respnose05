85efcab02194398bdde2f022da37838711c951c2
liuguanxing <173613742@qq.com>
2019-04-16 20:28:17 +0800

    第二次修改

--- src/com/git/test/Demo01.java
+++ src/com/git/test/Demo01.java
@@ -4,4 +4,7 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好");
     }
+    public void test(){
+        System.out.println("你是最棒的!");
+    }
 }
